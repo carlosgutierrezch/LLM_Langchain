d913d36ba73e0d6b8c84cffe88725cb3c05c52c9
Carlos <postcarloscook@gmail.com>
2024-08-24 20:59:53 +0200

    sql rag

--- notebooks/functions/cleaning_extracting.py
+++ notebooks/functions/cleaning_extracting.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import utm
+import requests
+import time
+
 #funcion para detectar columnas con valores NaN
 def list_of_columns_function(df):
     x=[]
@@ -44,3 +47,41 @@ def features_ing(total:pd.DataFrame)->pd.DataFrame:
     total['rango_de_precio']= np.random.randint(0,5,size=len(total))
     total['rating']= np.random.uniform(0,5,size=len(total)).round(1)
     return total
+
+def trial_function(coords:list):
+    df = []
+    round=0
+    # page=0
+    for lat, lng in coords:
+        url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=500&type=restaurant&type=bakery&type=bar&type=cafe&type=meal_delivery&type=meal_takeaway&key={API}"
+        params = {'pagetoken': ''}
+        while True:
+            response = requests.get(url, params)
+            if response.status_code != 200:
+                print("There is a problem, please confirm the requests")
+                break
+            data = response.json()
+            df.extend(data.get('results', []))
+            next_page_token = data.get('next_page_token')
+            if not next_page_token:
+                break
+            params['pagetoken'] = next_page_token
+            # print(f"page: {page}")
+            # page += 1
+            
+            time.sleep(2)
+        time.sleep(4)
+        round += 1
+        print(f'round {round}')
+        
+
+    return df
+
+def definitivo(*args)-> pd.DataFrame:
+    data_list = [trial_function(arg) for arg in args]
+    
+    data_frames = [pd.json_normalize(data) for data in data_list]
+    
+    final_data = pd.concat(data_frames, ignore_index=True)
+    
+    return final_data
